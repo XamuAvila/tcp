109a19766b365dd0ffbab15210c750d37baf5e1f
Samuel_Avila <samuel.avila@tekever.com>
2024-09-05 14:28:18 +0100

    subindo server

---
+++ server.py
@@ -0,0 +1,31 @@
+# rtsp-server.py
+from gi.repository import Gst, GstRtspServer, GObject
+import sys
+
+# Initialize GStreamer
+Gst.init(None)
+
+class RTSPServer(GstRtspServer.RTSPServer):
+    def __init__(self):
+        super().__init__()
+        self.set_service('8554')  # Port number for the RTSP server
+
+        # Create a media factory
+        self.factory = GstRtspServer.RTSPMediaFactory()
+        self.factory.set_launch(
+            "( filesrc location=input.ts ! tsdemux ! h264parse ! rtph264pay name=pay0 pt=96 )"
+        )
+        self.factory.set_shared(True)
+
+        # Create the mount point and attach the factory
+        self.get_mount_points().add_factory("/stream", self.factory)
+
+def main():
+    server = RTSPServer()
+    server.attach(None)
+
+    loop = GObject.MainLoop()
+    loop.run()
+
+if __name__ == '__main__':
+    main()
